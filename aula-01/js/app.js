87afa524008407d6f0a26d84da7e02422535e569
samuel farias <samuelcontatoe1@gmail.com>
2023-12-16 19:01:42 -0300

    add let, var e const

---
+++ aula-01/js/app.js
@@ -0,0 +1,48 @@
+/*let message = 'Hello!';
+
+alert(message); */
+
+// let user = 'John' , age = 25 , message = 'Hello!';
+
+//let user = 'John';
+//let age = 25;
+//let message = 'Hello!';
+
+/*let user = 'John';
+    age = 25;
+    message = 'Hello!';*/
+
+/*let message;
+message = 'Hello!';
+message = 'World!';
+alert(message)*/
+
+//let hello = 'Hello, World!'
+
+//let message;
+
+//message = hello;
+//alert(hello);
+//alert(message);
+
+//const myBirthday = '18.04.2023';
+
+/*let admin;
+let name;
+
+name = 'John';
+admin = name;
+
+alert(admin)*/
+
+//let planet = terra;
+//let userVisit = sam;
+
+/*const BIRTHDAY = '18.04.1982';
+
+const age = someCode(BIRTHDAY);
+alert(age);*/
+
+
+//nunbers
+
